fcca33d5f098ec6b93d8b03210c4af4529487bd7
SusanMamu <155472860+SusanMamu@users.noreply.github.com>
2024-02-26 11:46:09 +0300

    Add files via upload

---
+++ Power_Learn_Project/Python/Assignments/user_input.py
@@ -0,0 +1,5 @@
+name =  input("What is your name? ").title()
+age = input("What is your age? ")
+location = input("What is your location? ").title()
+
+print("Hello {}, you are {}, years old and live in {}".format(name,age,location))
